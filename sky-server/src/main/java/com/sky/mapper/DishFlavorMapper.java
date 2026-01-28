b227638a121d8e2083aa475f822df88d9fda55c5
admin_wu <13193589537@163.com>
2026-01-28 22:25:42 +0800

    菜品分页查询及删除

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,29 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口味
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 根据菜品ID删除口味
+     * @param
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId};")
+    void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品ID批量删除口味
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
+}
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,28 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+public interface DishService {
+    /**
+     * 新增菜品
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+}
